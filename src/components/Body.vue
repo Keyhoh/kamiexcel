a15046967a8f4227dde41472d16ebb55f16f19e8
Keyhoh <iheyhoh@gmail.com>
2019-02-27 01:04:18 +0900

    #1 add header and body components.

---
+++ src/components/Body.vue
@@ -0,0 +1,14 @@
+<template>
+    <!-- TODO: Implements abstract component. -->
+    <div>This is a body component.</div>
+</template>
+
+<script>
+    export default {
+        name: "Body"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+    <!-- TODO: Implements abstract component. -->
+    <div>This is a header component.</div>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+
+</style>
