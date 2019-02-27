12ee9bae5a9c0be534b3c3d4044baa72328dadc3
Keyhoh <iheyhoh@gmail.com>
2019-02-27 21:47:06 +0900

    Revert "#1 update app component. use header and body components."
    This reverts commit 8e9a49772828e69a7bcf253517ba2114a67b5809.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,28 @@
 <template>
-    <div id="app">
-        <Header></Header>
-        <Body></Body>
-    </div>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-    import Header from "./components/Header";
-    import Body from "./components/Body";
+import HelloWorld from './components/HelloWorld.vue'
 
-    export default {
-        name: 'app',
-        components: {
-            Body,
-            Header,
-        }
-    }
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
 </script>
 
 <style>
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
