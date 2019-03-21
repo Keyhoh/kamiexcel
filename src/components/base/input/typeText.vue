073ebc5446f921fc8f718633cf6f39b45da994c7
Keyhoh <iheyhoh@gmail.com>
2019-03-21 14:48:54 +0900

    create text type component

---
+++ src/components/base/input/typeText.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <input type="text" v-model="text">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "typeText",
+        props: {
+            value: {
+                text: String
+            }
+        },
+        data() {
+            return {
+                text: ""
+            }
+        },
+        updated() {
+            this.$emit("update:value", {
+                text: this.text
+            });
+        },
+        mounted() {
+            this.text = this.value.text;
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
