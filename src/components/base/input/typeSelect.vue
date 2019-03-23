46cfed6c6d76d310afd2223d8f34336bf55f87bb
Keyhoh <iheyhoh@gmail.com>
2019-03-23 17:19:30 +0900

    create input components

---
+++ src/components/base/input/typeSelect.vue
@@ -0,0 +1,23 @@
+<template>
+    <b-form-select v-model="selected" :options="items"/>
+</template>
+
+<script>
+    export default {
+        name: "typeList",
+        data() {
+            return {
+                selected: null,
+                items: [
+                    {value: null, text: 'Please select an option'},
+                    {value: 'a', text: 'This is First option'},
+                    {value: 'b', text: 'This is Second option'}
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
